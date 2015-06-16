6b84cc10cac060cb9579486848ad9c279090d361
Tim <timothy.r.koski@gmail.com>
2015-06-15 22:38:58 -0400

    Removed Library files and updated to Unity 5.1
    Updated Unity to 5.1 and had to clean up the Library from GitHub

---
+++ GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Windows.Input;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
