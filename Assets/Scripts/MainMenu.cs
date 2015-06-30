7fe561943ce52f04fbd94f4a047c1685f3274736
Tim <timothy.r.koski@gmail.com>
2015-06-29 23:07:08 -0400

    Created new 'game' scene
    Created a new scene for game objects. New scene called 'game' will be
    where all of the tech demo objects will exist.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,22 +2,31 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
     Canvas mainMenu;
     Text title;
     Text instruction;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         mainMenu = gameObject.GetComponent("MainMenuCanvas") as Canvas;
         instruction = (Text)GameObject.Find("Instructions").GetComponent(typeof(Text));
-        instruction = (Text)GameObject.Find("Title").GetComponent(typeof(Text));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.anyKey && !(Input.GetKeyUp("escape"))){
-            instruction.text = "Press 'Esc' to quit!";   
+        title = (Text)GameObject.Find("Title").GetComponent(typeof(Text));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
         }
-	}
+        else if (Input.anyKey)
+        {
+            Application.LoadLevel("game");
+            //instruction.text = "Press 'Esc' to quit!";
+        }
+    }
 }
